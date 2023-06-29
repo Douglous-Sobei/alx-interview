bb3839c9d7cdc5bec0ce96c5e096b9623a347bfe
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-29 07:03:57 +0300

    pascal  triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize with the first row [1]
+
+    for i in range(1, n):
+        row = [1]  # First element of each row is always 1
+
+        for j in range(1, i):
+            # Each element (except the first and last) in a row is the sum of the two elements above it
+            element = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.append(element)
+
+        row.append(1)  # Last element of each row is always 1
+        triangle.append(row)
+
+    return triangle
