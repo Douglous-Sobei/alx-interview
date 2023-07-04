e8c787628821052d90b7ab681759510062e52e85
Douglous Sobei <douglousmangoyi@gmail.com>
2023-07-05 02:10:51 +0300

    Update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+"""
+Problem: You have n number of locked boxes in front of you.
+         Each box is numbered sequentially from 0 to n - 1
+         and each box may contain keys to the other boxes.
+Task: Write a method that determines if all the boxes can be opened.
+"""
+
+
 def can_unlock_all(boxes):
     """
     Determines whether all the boxes can be unlocked.
