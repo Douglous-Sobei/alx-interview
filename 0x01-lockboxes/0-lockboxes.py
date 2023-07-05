d10cf025cabbaa7154a52060fb2cd1d987469741
Douglous Sobei <douglousmangoyi@gmail.com>
2023-07-05 03:32:19 +0300

    New correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,19 +8,18 @@ Task: Write a method that determines if all the boxes can be opened.
 """
 
 
-def can_unlock_all(boxes):
+def canUnlockAll(boxes):
     """
     Function that checks with boolean value if the list type and
     length to invoke two for iterations one to traverse the list
     and the other to compaer if key is idx or not in order to open
     """
-
     if type(boxes) is not list:
         return False
-    elif len(boxes) == 0:
+    elif (len(boxes)) == 0:
         return False
-    for n in range(1, len(boxes) -1):
-        boxex_checked = False
+    for n in range(1, len(boxes) - 1):
+        boxes_checked = False
         for idx in range(len(boxes)):
             boxes_checked = n in boxes[idx] and n != idx
             if boxes_checked:
--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-can_unlock_all = __import__('0-lockboxes').can_unlock_all
+canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
 boxes = [[1], [2], [3], [4], []]
-print(can_unlock_all(boxes))
+print(canUnlockAll(boxes))
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(can_unlock_all(boxes))
+print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(can_unlock_all(boxes))
+print(canUnlockAll(boxes))
